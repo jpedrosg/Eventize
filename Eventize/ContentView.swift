927de7ab9f6442085c37c77a8a6dc2de6c4b7075
JP <joaopedrogiarrante15@gmail.com>
2023-09-09 12:32:25 -0300

    Initial commit

---
+++ Eventize/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  Copyright © Uber Technologies, Inc. All rights reserved.
+//
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
